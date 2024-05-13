54ca6b76e56bf5d4c9b3c40954e93bf843c9429e
yacarthar <hoang.pham.vbhp@gmail.com>
2024-05-13 14:28:00 +0700

    rename docker script

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -17,6 +17,6 @@ echo \
   $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo apt-get install docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-compose-plugin
 sudo usermod -aG docker $USER
