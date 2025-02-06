428c6d9101bc32de0fcad7435a9c8322f730d4ef
yacarthar <hoang.pham.vbhp@gmail.com>
2025-02-06 16:11:30 +0700

    script: correct x86_64 ver of nvim

--- scripts/neovim_install.sh
+++ scripts/neovim_install.sh
@@ -1,34 +1,10 @@
 #!/bin/bash
 
-# Set variables
-NEOVIM_URL="https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-arm64.tar.gz"
-DOWNLOAD_DIR="$HOME/ds"
-INSTALL_DIR="/usr/local"
-SYMLINK_PATH="/usr/local/bin/nvim"
-TAR_FILE="$DOWNLOAD_DIR/nvim-linux64.tar.gz"
-
-# Create the download directory if it doesn't exist
-mkdir -p "$DOWNLOAD_DIR"
-
-# Download Neovim tar file
-echo "Downloading Neovim..."
-wget -O "$TAR_FILE" "$NEOVIM_URL"
-
-# Extract and move to /usr/local
-echo "Extracting Neovim..."
-tar -xzf "$TAR_FILE" -C "$DOWNLOAD_DIR"
-
-# Move extracted files to /usr/local
-echo "Installing Neovim..."
-sudo mv "$DOWNLOAD_DIR/nvim-linux64" "$INSTALL_DIR/nvim"
-
-# Create symlink
-echo "Creating symlink..."
-sudo ln -sf "$INSTALL_DIR/nvim/bin/nvim" "$SYMLINK_PATH"
-
-# Remove tar file
-echo "Cleaning up..."
-rm -f "$TAR_FILE"
-
-echo "Neovim installation complete!"
-
+mkdir -p ~/ds
+wget -O ~/ds/nvim.tar.gz https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz
+tar -xzf ~/ds/nvim.tar.gz -C ~/ds
+sudo rm -rf /usr/local/nvim
+sudo mkdir -p /usr/local/nvim
+sudo mv ~/ds/nvim-linux-x86_64/* /usr/local/nvim
+sudo ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/nvim
+rm -f ~/ds/nvim.tar.gz
