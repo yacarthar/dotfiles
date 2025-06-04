1a0e61b8874b78f0991ae1bb434c0489d7b02713
yacarthar <hoang.pham.vbhp@gmail.com>
2025-06-04 16:24:01 +0700

    vim: diagnostic; bash:ensurepath

--- scripts/neovim_install.sh
+++ scripts/neovim_install.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# install luarocks for lazy.nvim
+if ! which luarocks >/dev/null 2>&1; then
+    echo "Luarocks is not installed. Installing..."
+    sudo apt install -y luarocks
+    echo "Luarocks has been installed."
+else
+    echo "Luarocks is already installed."
+fi
+
+# python3-neovim and venv
+apt install -y python3-neovim
+PYTHON_VERSION=$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
+VENV_PACKAGE="python${PYTHON_VERSION}-venv"
+sudo apt install -y $VENV_PACKAGE
+
+if dpkg -l | grep -q $VENV_PACKAGE; then
+    echo "$VENV_PACKAGE installed successfully."
+else
+    echo "Failed to install $VENV_PACKAGE."
+fi
+
 mkdir -p ~/ds
 wget -O ~/ds/nvim.tar.gz https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz
 tar -xzf ~/ds/nvim.tar.gz -C ~/ds
